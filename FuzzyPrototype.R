05bb703f0531d8b0f5e434f98c8157a5232eac0b
@yurisa2 MT5 <yuri@sa2.com.br>
2018-10-09 15:33:05 -0300

    feature engineering all over

--- FuzzyPrototype.R
+++ FuzzyPrototype.R
@@ -1,81 +1,79 @@
-# PATH <- "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/FuzzySystem"
-PATH <- "/Applications/mampstack-5.6.21-2/apache2/htdocs/afis_fuzzysis"
+PATH <- "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/FuzzySystem"
+# PATH <- "/Applications/mampstack-5.6.21-2/apache2/htdocs/afis_fuzzysis"
 
 setwd(PATH)
 
 source(file="include.R")
 
-library(ggplot2)
 library(useful)
-library(corrplot)
-library(FuzzyR)
 
 options(warn=-1)
 
 data <- read.csv("dias_horas_full.csv")
-data$radiacao_global_kjm2  <- NULL
+data$radiacao_global_kjm2 <- NULL
 
 data$Dia <- as.Date(data$Dia)
 # data <- data[4000:5000,] # para nao fritar CPU
 
 # Data Por dia
-data_per_day <- aggregate(data[,4:ncol(data)], by=list(data$Dia), FUN=sum,na.rm=TRUE)
+data_per_day_sum <- aggregate(data[,4:ncol(data)], by=list(data$Dia), FUN=sum,na.rm=TRUE)
+data_per_day_mean <- aggregate(data[,4:ncol(data)], by=list(data$Dia), FUN=mean,na.rm=TRUE)
 
-data_per_day$temp_max_c <- NULL
-data_per_day$temp_min_c  <- NULL
-data_per_day$tem_max_pto_orvalho_c  <- NULL
-data_per_day$tempo_min_pto_orvalho_c  <- NULL
-data_per_day$umid_rel_ar_max_pctg  <- NULL
-data_per_day$umid_rel_ar_min_pctg  <- NULL
-data_per_day$pressao_atmosferica_max_hpa  <- NULL
-data_per_day$pressao_atmosferica_min_hpa  <- NULL
+colnames(data_per_day_sum) <- paste0("sum_",colnames(data_per_day_sum))
+colnames(data_per_day_mean) <- paste0("mean_",colnames(data_per_day_mean))
 
 data_per_day_max <- aggregate(data[,4:ncol(data)], by=list(data$Dia), FUN=max)
 data_per_day_min <- aggregate(data[,4:ncol(data)], by=list(data$Dia), FUN=min)
 data_per_day_deltas <- data_per_day_max - data_per_day_min
 
-data_per_day_deltas$temp_max_c <- NULL
-data_per_day_deltas$temp_min_c  <- NULL
-data_per_day_deltas$tem_max_pto_orvalho_c  <- NULL
-data_per_day_deltas$tempo_min_pto_orvalho_c  <- NULL
-data_per_day_deltas$umid_rel_ar_max_pctg  <- NULL
-data_per_day_deltas$umid_rel_ar_min_pctg  <- NULL
-data_per_day_deltas$pressao_atmosferica_max_hpa  <- NULL
-data_per_day_deltas$pressao_atmosferica_min_hpa  <- NULL
-data_per_day_deltas$precipitacao_mm  <- NULL
-data_per_day_deltas$delta_vento_raj_max_ms  <- NULL
 data_per_day_deltas[,1]  <- NULL
 
 colnames(data_per_day_deltas) <- paste0("delta_",colnames(data_per_day_deltas))
 
-data_per_day <- cbind(data_per_day,data_per_day_deltas)
-# str(data_per_day)
-# data_per_day <- tail(data_per_day, n=400)
+data_per_day <- cbind(data_per_day_mean,data_per_day_sum,data_per_day_deltas)
+
 # Shifts de resultado
-data_per_day <- shift.column(data=data_per_day, columns="precipitacao_mm", newNames="precipitacao_mm_1",len=1)
-data_per_day <- shift.column(data=data_per_day, columns="precipitacao_mm", newNames="precipitacao_mm_1_bin",len=1)
+data_per_day <- shift.column(data=data_per_day, columns="sum_precipitacao_mm", newNames="sum_precipitacao_mm_1",len=1)
+data_per_day <- shift.column(data=data_per_day, columns="sum_precipitacao_mm", newNames="sum_precipitacao_mm_1_bin",len=1)
 
 rownames(data_per_day) <- data_per_day[,1]
 data_per_day[,1] <- NULL
 
-data_per_day$precipitacao_mm_1_bin <- ifelse(data_per_day$precipitacao_mm_1_bin > 0 , 1, 0)
-data_per_day$precipitacao_mm_1_bin <- factor(data_per_day$precipitacao_mm_1_bin)
+data_per_day$sum_precipitacao_mm_1_bin <- ifelse(data_per_day$sum_precipitacao_mm_1_bin > 0 , 1, 0)
+data_per_day$sum_precipitacao_mm_1_bin <- factor(data_per_day$sum_precipitacao_mm_1_bin)
+
+
+
+# for (i in 1:ncol(data_per_day)){ print(paste(i,colnames(data_per_day)[i])) }
+
+#############
+
 
 # summary(data_per_day)
+# for (i in 1:ncol(data_per_day)){ print(paste(i,colnames(data_per_day)[i])) }
 # str(data_per_day[complete.cases(data_per_day),])
-
-# Data Por Mes
-
-data_per_month <- cbind(data,format(as.Date(data$Dia), "%m"))
-colnames(data_per_month)[ncol(data_per_month)] <- c("mes")
-data_per_month$mes <- factor(data_per_month$mes)
-data_per_month <- aggregate(data_per_month[,4:ncol(data_per_month)-1], by=list(data_per_month$mes), FUN=mean,na.rm=TRUE)
-data_per_month <- data_per_month[,c(1,3,4,5,12,13,14,16)]
+#
+# # Data Por Mes
+#
+# data_per_month <- cbind(data,format(as.Date(data$Dia), "%m"))
+# colnames(data_per_month)[ncol(data_per_month)] <- c("mes")
+# data_per_month$mes <- factor(data_per_month$mes)
+# data_per_month <- aggregate(data_per_month[,4:ncol(data_per_month)-1], by=list(data_per_month$mes), FUN=mean,na.rm=TRUE)
 
 # Data Setting
-n_col_features <- 1:15 # Define colunas para estudo;
-nbin <- 17 # Define a Coluna Binária
+n_col_features <- c(1,2,3,10,11,18,19,20,27,28,34,35,36,43,44) # Define colunas para estudo;
+nbin <- 51 # Define a Coluna Binária
 
-data_input <- data_per_day[sample(1:nrow(data_per_day),10),]
+# summary(data_per_day[complete.cases(data_per_day),n_col_features])
 
-result <- result_matrix(data_per_day,data_input,n_col_features,nbin, plots = F)
+data_input <- data_per_day[sample(1:nrow(data_per_day),50),]
+
+# result <- result_matrix(data_per_day,data_input,n_col_features,nbin)
+data_per_day <- data_per_day[complete.cases(data_per_day),]
+
+rm(acc1); rm(acc0); rm(acc_c); rm(acc_sc)
+
+acc1 <- accuracy_fis(data_per_day,data_input,n_col_features,nbin, method = "only_1", plots=T)
+acc0 <- accuracy_fis(data_per_day,data_input,n_col_features,nbin, method = "only_0")
+acc_c <- accuracy_fis(data_per_day,data_input,n_col_features,nbin, method = "conservative")
+acc_sc <- accuracy_fis(data_per_day,data_input,n_col_features,nbin, method = "sc")
--- FuzzyPrototype_m5.R
+++ FuzzyPrototype_m5.R
@@ -7,31 +7,42 @@ library(useful)
 
 options(warn=-1)
 
-
 winm5 <- read.csv("winm5.csv")
-winm5 <- winm5[sample(1:nrow(winm5),2000),]
+# winm5 <- winm5[sample(1:nrow(winm5),2000),]
 # colnames(winm5)
-
 
 winm5 <- shift.column(data=winm5, columns="lucro", newNames="lucro_1",len=1)
 
-winm5 <- winm5[which(winm5$direcao == 1),]
-
-
-winm5$lucro_1_bin <- ifelse(winm5$lucro_1 > 0,1 ,0)
+winm5$lucro_1_bin <- ifelse(winm5$lucro_1 > 0, 1, 0)
 
 # Data Setting
-n_col_features <- c(7:40,46:73) # Define colunas para estudo;
+winm5 <- winm5[which(winm5$direcao == 1),] #choose direction
+winm5_input <- winm5
 
+# Cols for model
+n_col_features <- c(7:40,46:73) # Define colunas para estudo;
+remove_cols <- c(10,22,33,34,59,70)
+n_col_features <- setdiff(n_col_features,remove_cols)
 nbin <- 75 # Define a Coluna Binária
 
+# Train Set for model
+train_size <- 200
+rand_start <- sample(train_size:nrow(winm5),1)
+rand_end <- rand_start + train_size
+# winm5 <- winm5[rand_start:rand_end,]
 
+# Test Set for model
+input_size <- 50
+input_start <- rand_end
+input_end <- input_size + input_start
+data_input <- winm5_input[input_start:input_end,]
 
-data_input <- winm5[sample(1:nrow(winm5),10),]
-# str(data_test)
-
-data_input <- data.matrix(data_input)
-
-
-result <- result_matrix(winm5,data_input,n_col_features,nbin, plots = T)
-# acc <- accuracy_fis(winm5,data_input,n_col_features,nbin)
+rm(acc1)
+rm(acc0)
+rm(acc_c)
+rm(acc_sc)
+# result <- result_matrix(winm5,data_input,n_col_features,nbin, plots = F)
+acc1 <- accuracy_fis(winm5,data_input,n_col_features,nbin, method = "only_1")
+acc0 <- accuracy_fis(winm5,data_input,n_col_features,nbin, method = "only_0")
+acc_c <- accuracy_fis(winm5,data_input,n_col_features,nbin, method = "conservative")
+acc_sc <- accuracy_fis(winm5,data_input,n_col_features,nbin, method = "sc")
